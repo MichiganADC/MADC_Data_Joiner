a051d8f999b28e66b3fced6a0d220f63818a9daa
L. D. Nicolas May <LDNicolasMay@gmail.com>
2019-07-10 15:19:51 -0400

    add download button; css styling

--- MADC_Data_Joiner/app.R
+++ MADC_Data_Joiner/app.R
@@ -1,45 +1,37 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# app.R
 
-library(shiny)
-library(readr)
-library(dplyr)
-library(stringr)
-library(DT)
-library(shinythemes)
+suppressMessages( library(shiny)       )
+suppressMessages( library(readr)       )
+suppressMessages( library(dplyr)       )
+suppressMessages( library(stringr)     )
+suppressMessages( library(DT)          )
+suppressMessages( library(shinythemes) )
 
 DT_OPTIONS <- list(pageLength = 5, 
                    lengthMenu = c(5, 10, 25, 50),
                    scrollX = TRUE)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
   
   # Set theme
   theme = shinytheme("flatly"),
-  # theme = "bootstrap.css",
+  # includeCSS("www/styles.css"),
   
   # Application title
-  # titlePanel("MADC Data Joiner",
-  #            windowTitle = "MADC Data Joiner"),
   titlePanel(
     tags$div(
       tags$h1("MADC Data Joiner", 
-              style = "font-weight:900;text-align:center;"),
-      style = "background-color:#ECF0F1;border-radius:5px;"
+              style = paste0("font-weight:900;",
+                             "text-align:center;",
+                             "padding-top:15px;",
+                             "padding-bottom:15px;")),
+      style = "background-color:#ecf0f1;border-radius:5px;"
     ),
     windowTitle = "MADC Data Joiner"
   ),
   
-  # hr(),
-  
-  # Sidebar with a slider input for number of bins 
+  # Sidebar
   sidebarLayout(
     sidebarPanel(
       width = 3,
@@ -97,48 +89,63 @@ ui <- fluidPage(
       fluidRow(
         column(width = 6,
                tags$div(
-                 tags$hr(),
                  tags$h1("X", style = "text-align:center;font-weight:900;"),
                  tags$hr(),
                  dataTableOutput(outputId = "data_x_out"), 
-                 tags$hr(),
-                 style = "background-color:#fff7f7;"
+                 style = paste0("background-color:#fff7f7;",
+                                "border-style:solid;",
+                                "border-radius:5px;",
+                                "border-width:1px;",
+                                "border-color:#ecf0f1;",
+                                "padding: 10px 10px 10px 10px;")
                )),
         column(width = 6,
                tags$div(
-                 tags$hr(),
                  tags$h1("Y", style = "text-align:center;font-weight:900;"),
                  tags$hr(),
                  dataTableOutput(outputId = "data_y_out"), 
-                 tags$hr(),
-                 style = "background-color:#f7fff7;"
+                 style = paste0("background-color:#f7fff7;",
+                                "border-style:solid;",
+                                "border-radius:5px;",
+                                "border-width:1px;",
+                                "border-color:#ecf0f1;",
+                                "padding: 10px 10px 10px 10px;")
                ))
       ),
       fluidRow(
         column(width = 12,
                tags$div(
-                 tags$hr(),
                  tags$h1("Z", style = "text-align:center;font-weight:900;"),
                  tags$hr(),
-                 dataTableOutput(outputId = "data_z_out"), 
+                 dataTableOutput(outputId = "data_z_out"),
                  tags$hr(),
-                 style = "background-color:#fffff7;"
+                 tags$div(
+                   downloadButton(outputId = "download_z",
+                                  label = "Download Z")
+                 ),
+                 style = paste0("background-color:#fffff7;",
+                                "border-style:solid;",
+                                "border-radius:5px;",
+                                "border-width:1px;",
+                                "border-color:#ecf0f1;",
+                                "margin-top:20px;",
+                                "margin-bottom:20px;",
+                                "padding: 10px 10px 10px 10px;")
                ))
-        # hr(), h1("Z"), hr(),
-        # dataTableOutput(outputId = "data_z_out"), hr() 
       )
     )
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
   
   # Get data
   
   df_data_x <- reactive({ 
-    df <- read.csv(file = input$data_x$datapath, stringsAsFactors = FALSE) %>% 
-      filter(stringr::str_detect(ptid, "^UM\\d{8}$"))
+    # df <- read.csv(file = input$data_x$datapath, stringsAsFactors = FALSE)
+    df <- read_csv(file = input$data_x$datapath,
+                   col_types = cols(.default = col_guess()))
     if (input$ren_switch == "no") {
       return(select(df, -redcap_event_name))
     } else {
@@ -147,9 +154,9 @@ server <- function(input, output) {
   })
   
   df_data_y <- reactive({ 
-    df <- read.csv(file = input$data_y$datapath, stringsAsFactors = FALSE) %>% 
-      filter(stringr::str_detect(subject_id, "^UM\\d{8}$")) %>% 
-      filter(subject_id >= "UM00000543")
+    # df <- read.csv(file = input$data_y$datapath, stringsAsFactors = FALSE)
+    df <- read_csv(file = input$data_y$datapath,
+                   col_types = cols(.default = col_guess()))
     if (input$ren_switch == "no") {
       return(select(df, -redcap_event_name))
     } else {
@@ -167,7 +174,7 @@ server <- function(input, output) {
       str_trim()
   })
   
-  df_data_z <- tibble()
+  # df_data_z <- tibble()
   
   df_data_z <- reactive({
     
@@ -202,17 +209,19 @@ server <- function(input, output) {
   
   # Outputs
   
+  # X data table
   output$data_x_out <- renderDataTable({
     req(input$data_x)
     datatable(df_data_x(), DT_OPTIONS)
   })
   
-  
+  # Y data table
   output$data_y_out <- renderDataTable({
     req(input$data_y)
     datatable(df_data_y(), DT_OPTIONS)
   })
   
+  # Z data table
   output$data_z_out <- renderDataTable({
     req(input$data_x, input$data_y,
         x_join_fields(), y_join_fields(),
@@ -220,6 +229,15 @@ server <- function(input, output) {
         y_join_fields() %in% names(df_data_y()))
     datatable(df_data_z(), DT_OPTIONS)
   })
+  
+  # Z data download
+  output$download_z <- downloadHandler(
+    filename = function() { "Z_data.csv" },
+    content = function(file) {
+      write_csv(df_data_z(), file, na = "")
+    }
+  )
+  
   
 }
 
